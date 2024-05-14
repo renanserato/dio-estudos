81d8bce17a6a5fc9b85699a39019dd6966711ddd
Renanserato <renan_serato@hotmail.com>
2024-05-14 14:42:05 -0300

    Estruturas Condicionais

---
+++ Estruturas Condicionais/estruturas-condicionais/src/Estruturas.java
@@ -0,0 +1,20 @@
+public class Estruturas {
+    public static void main(String[] args) {
+        
+        int a = 0;
+        int b = 2;
+
+        if(a==b){
+            System.out.println("Verdadeiro");
+        } else if (a == 0 || b == 0) {
+            System.out.println("Fim");
+        } else {
+            System.out.println("Falso");
+        }
+        }
+    }
+
+
+    /*
+     Estruturas condicionais if e else. Operadores && (e), || (ou).
+     */
